3f00a4c0f4f910027cad215f529d3a1fa8e62963
Aliaksandr <casha.shura@gmail.com>
2023-03-30 18:09:41 +0300

    fixed axios

--- src/redux/store.ts
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cardSlice from "./card/cardSlice";
-
-export const store = configureStore({
-  reducer: {
-    cardData: cardSlice,
-  },
-});
